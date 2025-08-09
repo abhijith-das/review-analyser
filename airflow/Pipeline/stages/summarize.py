6ef4df1c73267a871f3427bcc6039918cefded68
abhijith-das <dasabhijith01@gmail.com>
2025-08-09 16:07:05 +0530

    added llm calls

--- airflow/Pipeline/stages/summarize.py
+++ airflow/Pipeline/stages/summarize.py
@@ -1,6 +1,14 @@
+import json
+import random
+import time
+import re
 import numpy as np
 import pandas as pd
 from utils.db_operations import retrieve_table_as_df, retrieve_values_closest_to_centroid, create_table_from_df
+from google import genai
+from google.genai import types
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
 
 # function to retrieve all reviews from the database
 def get_reviews_from_db(db_name: str, schema_name: str, table_name: str) -> pd.DataFrame:
@@ -35,14 +43,131 @@ def get_reviews_for_llm(df_closest: pd.DataFrame, df_all: pd.DataFrame) -> pd.Da
     df_reviews_for_cluster = pd.DataFrame(reviews_for_cluster)
     return df_reviews_for_cluster
 
+# function to summarize reviews 
+
+def summarize_reviews(cluster_id, cluster_texts, api_key, max_retries=5, base_delay=1, max_delay=10):
+    client = genai.Client(api_key=api_key)
+
+    prompt = f"""
+    You are given a set of product reviews belonging to the same cluster.
+
+    Cluster ID: {cluster_id}
+    Reviews:
+    {cluster_texts}
+
+    Task:
+    1. Generate a short, catchy title (max 6 words) summarizing the main theme.
+    2. Write a concise 2-3 sentence description summarizing the main idea of this cluster.
+    Format the response as:
+    Title: <title>
+    Description: <description>
+    """
+  
+    # response = client.models.generate_content(
+    #     model = "gemma-3-27b-it",
+    #     contents = prompt,
+    #     config = types.GenerateContentConfig(
+    #         temperature=0.1,
+    #         top_p=0.5,
+    #         top_k=20
+    #     )
+    # )
+
+    # print(response.text)
+
+
+    for attempt in range(max_retries):
+        try:
+            response = client.models.generate_content(
+                model="gemma-3-27b-it",
+                contents=prompt,
+                config=types.GenerateContentConfig(
+                    temperature=0.1,
+                    top_p=0.5,
+                    top_k=20
+                )
+            )
+            text_output = response.text.strip()
+            print(f"Response: {text_output}")
+            title_match = re.search(r"Title:\s*(.*)", text_output)
+            desc_match = re.search(r"Description:\s*(.*)", text_output, re.DOTALL)
+
+            title = title_match.group(1).strip() if title_match else ""
+            description = desc_match.group(1).strip() if desc_match else ""
+
+            return title, description
+
+        except Exception as e:
+            wait_time = min(max_delay, base_delay * (2 ** attempt))  
+            wait_time += random.uniform(0, 1)  
+            print(f"[Attempt {attempt+1}/{max_retries}] Error: {e} — retrying in {wait_time:.2f}s...")
+            time.sleep(wait_time)
+
+# function to help threading
+def process_cluster(cluster_id, df_reviews_for_llm, api_key):
+    max_retries=5
+    base_delay=1
+    max_delay=10
+    cluster_reviews = df_reviews_for_llm[df_reviews_for_llm['cluster_id'] == cluster_id]['review']
+    cluster_texts = "\n".join(cluster_reviews.tolist())
+    summary = summarize_reviews(cluster_id, cluster_texts, api_key, max_retries, base_delay, max_delay)
+    return {
+        "cluster_id": cluster_id,
+        "summary": summary
+    }
 
 # main function to retrieve reviews and prepare them for LLM processing
 def summarize_and_sentiment_analysis(db_name: str, schema_name: str, table_name: str):
+
+    with open('./stages/credentials.json') as f:
+        credentials = json.load(f)
+        api_key = credentials['gemini']['api_key']
+
     df_all_reviews = get_reviews_from_db(db_name, schema_name, table_name)
     df_closest_reviews = get_reviews_closest_to_centroid(db_name, schema_name, table_name)
     df_random_reviews_for_llm = get_reviews_for_llm(df_closest_reviews, df_all_reviews)
-    df_reviews_for_llm = pd.concat([df_closest_reviews, df_random_reviews_for_llm], ignore_index=True).drop_duplicates()
+    df_reviews_for_llm = pd.concat(
+        [df_closest_reviews, df_random_reviews_for_llm], 
+        ignore_index=True
+        ).drop_duplicates()
 
     print(df_reviews_for_llm)
 
-    create_table_from_df(df_reviews_for_llm, 'reviews_for_llm')
+    # create_table_from_df(df_reviews_for_llm, 'reviews_for_llm')
+
+    cluster_summaries = []
+
+    cluster_ids = df_reviews_for_llm['cluster_id'].unique()
+
+
+    # cluster_summaries = []
+    for cluster_id in df_reviews_for_llm['cluster_id'].unique():
+        cluster_reviews = df_reviews_for_llm[df_reviews_for_llm['cluster_id'] == cluster_id]['review']
+        cluster_texts = "\n".join(cluster_reviews.tolist())
+        
+        title, description = summarize_reviews(cluster_id, cluster_texts, api_key)
+        cluster_summaries.append({
+            "cluster_id": cluster_id,
+            "title": title,
+            "description": description
+        })
+    # # Convert summaries to a DataFrame and save
+   
+    # with ThreadPoolExecutor(max_workers=15) as executor:
+    #     futures = {
+    #         executor.submit(process_cluster, cid, df_reviews_for_llm, api_key): cid
+    #         for cid in cluster_ids
+    #     }
+    #     for future in as_completed(futures):
+    #         cid = futures[future]
+    #         try:
+    #             result = future.result()
+    #             cluster_summaries.append(result)
+    #         except Exception as e:
+    #             print(f"Error processing cluster {cid}: {e}")
+
+    df_summaries = pd.DataFrame(cluster_summaries)
+    print(df_summaries)
+
+
+    create_table_from_df(df_summaries, 'cluster_summaries')
